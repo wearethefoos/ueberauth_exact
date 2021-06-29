f806b1c27565105d1849e899d5bde529e58deca5
Wouter de Vos <309331+wearethefoos@users.noreply.github.com>
2021-06-29 10:42:36 +0200

    New project

---
+++ lib/ueberauth_exact.ex
@@ -0,0 +1,18 @@
+defmodule UeberauthExact do
+  @moduledoc """
+  Documentation for `UeberauthExact`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UeberauthExact.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ueberauth_exact_test.exs
@@ -0,0 +1,8 @@
+defmodule UeberauthExactTest do
+  use ExUnit.Case
+  doctest UeberauthExact
+
+  test "greets the world" do
+    assert UeberauthExact.hello() == :world
+  end
+end
